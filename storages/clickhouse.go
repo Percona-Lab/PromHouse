aab89733f77a66afd1430817ea1bced26f062b77
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2017-08-20 16:01:24 +0300

    Store metrics separately.

--- storages/clickhouse.go
+++ storages/clickhouse.go
@@ -21,6 +21,7 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	_ "github.com/kshvakov/clickhouse" // register SQL driver
@@ -42,8 +43,11 @@ type ClickHouse struct {
 	db       *sql.DB
 	l        *logrus.Entry
 	database string
-	// labels   []string
 
+	metrics   map[model.Fingerprint]model.Metric
+	metricsRW sync.RWMutex
+
+	mMetricsCurrent prometheus.Gauge
 	mSamplesCurrent prometheus.Gauge
 
 	mReads      prometheus.Summary
@@ -52,6 +56,7 @@ type ClickHouse struct {
 	mWrites         prometheus.Summary
 	mWriteErrors    *prometheus.CounterVec
 	mWrittenLabels  *prometheus.CounterVec
+	mWrittenMetrics prometheus.Counter
 	mWrittenSamples prometheus.Counter
 }
 
@@ -86,6 +91,14 @@ func NewClickHouse(dsn string, database string, init bool) (*ClickHouse, error) 
 		)
 		ENGINE = MergeTree(__date__, (__fingerprint__, __ts__), 8192)`, database))
 
+	queries = append(queries, fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS %s.metrics (
+			date Date,
+			fingerprint UInt64,
+			labels String
+		)
+		ENGINE = MergeTree(date, fingerprint, 8192)`, database))
+
 	for _, q := range queries {
 		l.Infof("Executing: %s", q)
 		if _, err = db.Exec(q); err != nil {
@@ -97,8 +110,15 @@ func NewClickHouse(dsn string, database string, init bool) (*ClickHouse, error) 
 		db:       db,
 		l:        l,
 		database: database,
-		// labels:   labels,
 
+		metrics: make(map[model.Fingerprint]model.Metric, 8192),
+
+		mMetricsCurrent: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: subsystem,
+			Name:      "metrics_current",
+			Help:      "Current number of stored metrics.",
+		}),
 		mSamplesCurrent: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Subsystem: subsystem,
@@ -137,6 +157,12 @@ func NewClickHouse(dsn string, database string, init bool) (*ClickHouse, error) 
 			Name:      "written_labels",
 			Help:      "Number of written labels by name.",
 		}, []string{"name"}),
+		mWrittenMetrics: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: subsystem,
+			Name:      "written_metrics",
+			Help:      "Number of written metrics.",
+		}),
 		mWrittenSamples: prometheus.NewCounter(prometheus.CounterOpts{
 			Namespace: namespace,
 			Subsystem: subsystem,
@@ -153,7 +179,17 @@ func NewClickHouse(dsn string, database string, init bool) (*ClickHouse, error) 
 	return ch, nil
 }
 
+// makeMetric converts a slice of labels to a metric.
+func makeMetric(labels []*prompb.Label) model.Metric {
+	metric := make(model.Metric, len(labels))
+	for _, l := range labels {
+		metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+	}
+	return metric
+}
+
 func (ch *ClickHouse) Describe(c chan<- *prometheus.Desc) {
+	ch.mMetricsCurrent.Describe(c)
 	ch.mSamplesCurrent.Describe(c)
 
 	ch.mReads.Describe(c)
@@ -162,18 +198,27 @@ func (ch *ClickHouse) Describe(c chan<- *prometheus.Desc) {
 	ch.mWrites.Describe(c)
 	ch.mWriteErrors.Describe(c)
 	ch.mWrittenLabels.Describe(c)
+	ch.mWrittenMetrics.Describe(c)
 	ch.mWrittenSamples.Describe(c)
 }
 
 func (ch *ClickHouse) Collect(c chan<- prometheus.Metric) {
-	query := fmt.Sprintf("SELECT COUNT(*) FROM %s.samples", ch.database)
 	var count uint64
+	query := fmt.Sprintf("SELECT COUNT(*) FROM %s.metrics", ch.database)
+	if err := ch.db.QueryRow(query).Scan(&count); err != nil {
+		ch.l.Error(err)
+		return
+	}
+	ch.mMetricsCurrent.Set(float64(count))
+
+	query = fmt.Sprintf("SELECT COUNT(*) FROM %s.samples", ch.database)
 	if err := ch.db.QueryRow(query).Scan(&count); err != nil {
 		ch.l.Error(err)
 		return
 	}
 	ch.mSamplesCurrent.Set(float64(count))
 
+	ch.mMetricsCurrent.Collect(c)
 	ch.mSamplesCurrent.Collect(c)
 
 	ch.mReads.Collect(c)
@@ -182,6 +227,7 @@ func (ch *ClickHouse) Collect(c chan<- prometheus.Metric) {
 	ch.mWrites.Collect(c)
 	ch.mWriteErrors.Collect(c)
 	ch.mWrittenLabels.Collect(c)
+	ch.mWrittenMetrics.Collect(c)
 	ch.mWrittenSamples.Collect(c)
 }
 
@@ -279,17 +325,28 @@ func (ch *ClickHouse) Read(ctx context.Context, queries []Query) (res *prompb.Re
 	return
 }
 
+func inTransaction(ctx context.Context, db *sql.DB, f func(*sql.Tx) error) (err error) {
+	var tx *sql.Tx
+	if tx, err = db.BeginTx(ctx, nil); err != nil {
+		return
+	}
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		} else {
+			tx.Rollback()
+		}
+	}()
+	err = f(tx)
+	return
+}
+
 func (ch *ClickHouse) Write(ctx context.Context, data *prompb.WriteRequest) (err error) {
 	start := time.Now()
-	var tx *sql.Tx
 	defer func() {
 		if err == nil {
 			ch.mWrites.Observe(time.Since(start).Seconds())
 			return
-		}
-
-		if tx != nil {
-			tx.Rollback()
 		}
 
 		t := "other"
@@ -299,59 +356,92 @@ func (ch *ClickHouse) Write(ctx context.Context, data *prompb.WriteRequest) (err
 		ch.mWriteErrors.WithLabelValues(t).Inc()
 	}()
 
-	tx, err = ch.db.BeginTx(ctx, nil)
+	// make metrics and calculate fingerprints
+	metrics := make(map[model.Fingerprint]model.Metric, len(data.Timeseries))
+	for _, ts := range data.Timeseries {
+		m := makeMetric(ts.Labels)
+		metrics[m.Fingerprint()] = m
+	}
+
+	// find new metrics
+	newMetrics := make([]model.Fingerprint, 0, len(metrics))
+	ch.metricsRW.Lock()
+	for f, m := range metrics {
+		_, ok := ch.metrics[f]
+		if !ok {
+			newMetrics = append(newMetrics, f)
+			ch.metrics[f] = m
+		}
+	}
+	ch.metricsRW.Unlock()
+
+	// write new metrics
+	err = inTransaction(ctx, ch.db, func(tx *sql.Tx) error {
+		placeholders := strings.Repeat("?, ", 3)
+		query := fmt.Sprintf(
+			`INSERT INTO %s.metrics (date, fingerprint, labels) VALUES (%s)`,
+			ch.database, placeholders[:len(placeholders)-2], // cut last ", "
+		)
+		var stmt *sql.Stmt
+		if stmt, err = tx.PrepareContext(ctx, query); err != nil {
+			return err
+		}
+
+		args := make([]interface{}, 3)
+		args[0] = model.Now().Time()
+		for _, f := range newMetrics {
+			args[1] = uint64(f)
+			args[2] = util.MarshalMetric(metrics[f])
+			if _, err = stmt.ExecContext(ctx, args...); err != nil {
+				return err
+			}
+		}
+
+		return stmt.Close()
+	})
 	if err != nil {
 		return
 	}
 
-	// columns := 4 + len(ch.labels)
-	const columns = 5
-	placeholders := strings.Repeat("?, ", columns)
-	query := fmt.Sprintf(
-		`INSERT INTO %s.samples (__labels__, __fingerprint__, __ts__, __value__, __date__) VALUES (%s)`,
-		ch.database, placeholders[:len(placeholders)-2], // cut last ", "
-	)
-
-	var stmt *sql.Stmt
-	if stmt, err = tx.PrepareContext(ctx, query); err != nil {
-		return err
-	}
-	defer func() {
-		e := stmt.Close()
-		if err == nil {
-			err = e
-		}
-	}()
-
+	// write samples
 	var samples int
-	for _, ts := range data.Timeseries {
-		args := make([]interface{}, columns)
-
-		metric := make(model.Metric, len(ts.Labels))
-		for _, l := range ts.Labels {
-			metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
-			ch.mWrittenLabels.WithLabelValues(l.Name).Inc()
+	err = inTransaction(ctx, ch.db, func(tx *sql.Tx) error {
+		placeholders := strings.Repeat("?, ", 5)
+		query := fmt.Sprintf(
+			`INSERT INTO %s.samples (__labels__, __fingerprint__, __ts__, __value__, __date__) VALUES (%s)`,
+			ch.database, placeholders[:len(placeholders)-2], // cut last ", "
+		)
+		var stmt *sql.Stmt
+		if stmt, err = tx.PrepareContext(ctx, query); err != nil {
+			return err
 		}
-		args[0] = util.MarshalMetric(metric)
-		args[1] = uint64(metric.Fingerprint())
 
-		for _, s := range ts.Samples {
-			args[len(args)-3] = s.Timestamp
-			args[len(args)-2] = s.Value
-			args[len(args)-1] = model.Time(s.Timestamp).Time()
-			_, err = stmt.ExecContext(ctx, args...)
-			if err != nil {
-				return
+		args := make([]interface{}, 5)
+		for _, ts := range data.Timeseries {
+			m := makeMetric(ts.Labels)
+			args[0] = util.MarshalMetric(m)
+			args[1] = uint64(m.Fingerprint())
+
+			for _, s := range ts.Samples {
+				args[len(args)-3] = s.Timestamp
+				args[len(args)-2] = s.Value
+				args[len(args)-1] = model.Time(s.Timestamp).Time()
+				if _, err = stmt.ExecContext(ctx, args...); err != nil {
+					return err
+				}
+				samples++
 			}
-			samples++
 		}
-	}
 
-	if err = tx.Commit(); err != nil {
+		return stmt.Close()
+	})
+	if err != nil {
 		return
 	}
+
+	ch.mWrittenMetrics.Add(float64(len(newMetrics)))
 	ch.mWrittenSamples.Add(float64(samples))
-	ch.l.Debugf("Wrote %d samples.", samples)
+	ch.l.Debugf("Wrote %s new metrics, %d samples.", len(newMetrics), samples)
 	return
 }
 

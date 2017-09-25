01efc12b55d3d1e218fe1c3c536626f2e0b8ab50
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2017-09-25 20:57:30 +0300

    Update Docker Compose version.

--- misc/setup.sh
+++ misc/setup.sh
@@ -13,7 +13,7 @@ sudo add-apt-repository -u "deb [arch=amd64] https://download.docker.com/linux/u
 sudo apt install -y docker-ce
 sudo docker --version
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
 

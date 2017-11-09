a7f792035442e3a3e8774cab74e2494f83b29a01
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2017-11-09 10:35:17 +0300

    Update deps.

--- prompb/vendor.sh
+++ prompb/vendor.sh
@@ -7,13 +7,13 @@ git clone https://github.com/prometheus/prometheus.git prom
 
 rm -fr prom1
 mkdir prom1
-cd prom && git checkout v1.7.1
+cd prom && git checkout v1.8.2
 cp -v storage/remote/*.proto ../prom1
 cd ..
 
 rm -fr prom2
 mkdir prom2
-cd prom && git checkout v2.0.0-beta.5
+cd prom && git checkout v2.0.0
 cp -v prompb/*.proto ../prom2
 cd ..
 
